2f7e4d15317733a04f507f9d82f77e2489482758
Your Name <you@example.com>
2020-12-14 10:27:46 -0700

    update parsing from correct source

--- main.py
+++ main.py
@@ -4,14 +4,20 @@ import json
 from lib.utm import Convert_LL2UTM as utm
 
 loc = []
-
-os.system("adb shell dumpsys location  | grep 'last location' > %s"%("data/data.txt"))
-pattern = re.compile(r"(?<=k)(.*?)(?=h)")
+stringMe = "Gnss"
+#| grep 'last location'
+os.system("adb shell dumpsys location | grep %s  > %s"%(stringMe,"data/data.txt"))
+latPat = re.compile(r"(?<=LatitudeDegrees:)(.*?)(?=,)")
+lonPat = re.compile(r"(?<=LongitudeDegrees:)(.*?)(?=,)")
 
 for line in open("data/data.txt"):
-    for match in re.findall(pattern, line):
+    for match in re.findall(latPat, line):
         loc.append(match)
-x = loc[0]
-# print(str(x).split(","))
-utms = utm(str(x).split(","))
-print(utms)
+
+for lines in open("data/data.txt"):
+    for matchs in re.findall(lonPat,lines):
+        loc.append(matchs)
+x = loc
+print(x)
+utms = utm(x)
+print("easting ",utms[2]," northing ",utms[3])
