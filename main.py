6bec5b9d2deda48cb4a2a0d9df58a69a733d22e8
Your Name <you@example.com>
2020-12-14 08:58:19 -0700

    update pattern match for lat lon

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import os
+import re
 import json
 
-os.system("adb shell dumpsys location > %s"%("data/data.txt"))
+loc = []
+
+os.system("adb shell dumpsys location  | grep 'last location' > %s"%("data/data.txt"))
+pattern = re.compile(r"(?<=k)(.*?)(?=h)")
+
+for line in open("data/data.txt"):
+    for match in re.findall(pattern, line):
+        loc.append(match)
+x = loc[0]
+print(str(x).split(","))
