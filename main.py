b65f4adfe0a258a997754795db420d6cd13d542c
Your Name <you@example.com>
2020-12-14 09:02:03 -0700

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import re
 import json
+from lib.utm import Convert_LL2UTM
 
 loc = []
 
@@ -11,4 +12,6 @@ for line in open("data/data.txt"):
     for match in re.findall(pattern, line):
         loc.append(match)
 x = loc[0]
-print(str(x).split(","))
+# print(str(x).split(","))
+utms = Convert_LL2UTM(str(x).split(","))
+print(utms)
