87477e152ba793b9a180c6f7619c91b8dc88f410
Your Name <you@example.com>
2020-12-14 21:45:20 -0700

    update

--- main.py
+++ main.py
@@ -4,20 +4,29 @@ import json
 from lib.utm import Convert_LL2UTM as utm
 
 loc = []
-stringMe = "Gnss"
+stringMe = "fused"
 #| grep 'last location'
-os.system("adb shell dumpsys location | grep %s  > %s"%(stringMe,"data/data.txt"))
-latPat = re.compile(r"(?<=LatitudeDegrees:)(.*?)(?=,)")
-lonPat = re.compile(r"(?<=LongitudeDegrees:)(.*?)(?=,)")
+# os.system("adb shell dumpsys location > %s"%("data/ex.txt"))
 
-for line in open("data/data.txt"):
-    for match in re.findall(latPat, line):
-        loc.append(match)
+def gps_call():
+    while True:
+        os.system("adb shell dumpsys location | grep %s  > %s"%(stringMe,"data/data.txt"))
+        latPat = re.compile(r"(?<=d)(.*?)(?=h)")
+        # lonPat = re.compile(r"(?<=LongitudeDegrees:)(.*?)(?=,)")
 
-for lines in open("data/data.txt"):
-    for matchs in re.findall(lonPat,lines):
-        loc.append(matchs)
-x = loc
-print(x)
-utms = utm(x)
-print("easting ",utms[2]," northing ",utms[3])
+        for line in open("data/data.txt"):
+            for match in re.findall(latPat, line):
+                loc.append(match)
+        x = loc[1]
+        print(x.split(','))
+        # for lines in open("data/data.txt"):
+        #     for matchs in re.findall(lonPat,lines):
+        #         print(match,matchs)
+                # loc.append(matchs)
+            # x = loc
+            # print(loc)
+            # print(x)    
+            # utms = utm(x)
+    # print("easting ",utms[2]," northing ",utms[3])
+
+gps_call()
